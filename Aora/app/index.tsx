ec9fd2ec8c45ac11415d66adf49854e6851d5895
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-06-25 20:49:59 -0700

    saving progress for the day

--- Aora/app/index.tsx
+++ Aora/app/index.tsx
@@ -3,15 +3,16 @@ import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { styled } from "nativewind";
 
-// const StyledView = styled(View);
+const StyledView = styled(View);
+const StyledText = styled(Text);
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-red-500">
-        <Text className="text-3xl">Aora!</Text>
+    <StyledView className="flex-1 items-center justify-center bg-red-500">
+        <StyledText className="text-3xl">Aora!</StyledText>
         <StatusBar />
         <Link href='/profile' style={{ color: 'blue' }}>Go to Profile</Link>
-    </View>
+    </StyledView>
   );
 }
 
