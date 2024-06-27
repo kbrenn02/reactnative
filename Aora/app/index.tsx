7865fd77431cf27276710bc1cdaa858e3fc10f76
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-06-26 20:58:58 -0700

    Set up structure of the app

--- Aora/app/index.tsx
+++ Aora/app/index.tsx
@@ -5,7 +5,7 @@ import { StatusBar } from 'expo-status-bar';
 export default function HomeScreen() {
   return (
     <View className='flex-1 justify-center items-center bg-white'>
-        <Text className='text-3xl'>Aora!</Text>
+        <Text className='text-3xl font-pblack'>Aora!</Text>
         <StatusBar />
         <Link href='/profile' style={{ color: 'blue' }}>Go to Profile</Link>
     </View>
