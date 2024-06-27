836e766f1e1c3928528863ef531c1d941bb053d7
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-06-26 21:05:51 -0700

    Omg. Nativewind works

--- Aora/app/index.tsx
+++ Aora/app/index.tsx
@@ -3,11 +3,11 @@ import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 
 export default function HomeScreen() {
-  return (
-    <View className='flex-1 justify-center items-center bg-white'>
-        <Text className='text-3xl font-pblack'>Aora!</Text>
-        <StatusBar />
-        <Link href='/profile' style={{ color: 'blue' }}>Go to Profile</Link>
-    </View>
-  );
+    return (
+        <View className='flex-1 justify-center items-center bg-white'>
+            <Text className='text-3xl font-pblack'>Aora!</Text>
+            <StatusBar />
+            <Link href='/profile' style={{ color: 'blue' }}>Go to Profile</Link>
+        </View>
+    );
 }
