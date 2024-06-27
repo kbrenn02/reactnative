9c36987e4b7f0141313c3d549b7ee1cb0cf3c574
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-06-26 20:50:38 -0700

    Configure fonts in the _layout file

--- Aora/app/index.tsx
+++ Aora/app/index.tsx
@@ -4,20 +4,10 @@ import { StatusBar } from 'expo-status-bar';
 
 export default function HomeScreen() {
   return (
-    <View className='flex-1 justify-center items-center bg-gray-500'>
-        <Text>Aora!</Text>
+    <View className='flex-1 justify-center items-center bg-white'>
+        <Text className='text-3xl'>Aora!</Text>
         <StatusBar />
         <Link href='/profile' style={{ color: 'blue' }}>Go to Profile</Link>
-        <Text>Another text because i need to actually see it on the screen and if i make it longer with blah blah</Text>
     </View>
   );
 }
-
-
-// No overload matches this call.
-//   Overload 1 of 2, '(props: ViewProps): View', gave the following error.
-//     Type '{ children: Element[]; className: string; }' is not assignable to type 'IntrinsicAttributes & IntrinsicClassAttributes<View> & Readonly<ViewProps>'.
-//       Property 'className' does not exist on type 'IntrinsicAttributes & IntrinsicClassAttributes<View> & Readonly<ViewProps>'.
-//   Overload 2 of 2, '(props: ViewProps, context: any): View', gave the following error.
-//     Type '{ children: Element[]; className: string; }' is not assignable to type 'IntrinsicAttributes & IntrinsicClassAttributes<View> & Readonly<ViewProps>'.
-//       Property 'className' does not exist on type 'IntrinsicAttributes & IntrinsicClassAttributes<View> & Readonly<ViewProps>'.
--- Aora/tailwind.config.js
+++ Aora/tailwind.config.js
@@ -1,4 +1,4 @@
-// /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
     theme: {
