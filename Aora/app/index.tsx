7a18a4b2bca9e7844636adee1916812240395da3
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-06-26 20:30:59 -0700

    added a file to get rid of the errors but the styling itself still isnt working

--- Aora/app/index.tsx
+++ Aora/app/index.tsx
@@ -1,19 +1,23 @@
 import { View, Text } from 'react-native';
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { styled } from 'nativewind';
-
-const StyledView = styled(View)
-
 
 export default function HomeScreen() {
   return (
-    <StyledView className='flex-1 items-center justify-center bg-red-500'>
+    <View className='flex-1 justify-center items-center bg-gray-500'>
         <Text>Aora!</Text>
         <StatusBar />
         <Link href='/profile' style={{ color: 'blue' }}>Go to Profile</Link>
-    </StyledView>
+        <Text>Another text because i need to actually see it on the screen and if i make it longer with blah blah</Text>
+    </View>
   );
 }
 
 
+// No overload matches this call.
+//   Overload 1 of 2, '(props: ViewProps): View', gave the following error.
+//     Type '{ children: Element[]; className: string; }' is not assignable to type 'IntrinsicAttributes & IntrinsicClassAttributes<View> & Readonly<ViewProps>'.
+//       Property 'className' does not exist on type 'IntrinsicAttributes & IntrinsicClassAttributes<View> & Readonly<ViewProps>'.
+//   Overload 2 of 2, '(props: ViewProps, context: any): View', gave the following error.
+//     Type '{ children: Element[]; className: string; }' is not assignable to type 'IntrinsicAttributes & IntrinsicClassAttributes<View> & Readonly<ViewProps>'.
+//       Property 'className' does not exist on type 'IntrinsicAttributes & IntrinsicClassAttributes<View> & Readonly<ViewProps>'.
