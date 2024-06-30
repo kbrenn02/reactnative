5d3285c96c4bf122ddbb86140283bebafd6cc57b
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-06-29 21:43:40 -0700

    Finish setting up appwrite IDs for projects, db, collections, and storage

--- Aora/lib/appwrite.ts
+++ Aora/lib/appwrite.ts
@@ -2,5 +2,8 @@ export const appwriteConfig = {
     endpoint: 'https://cloud.appwrite.io/v1',
     platform: 'com.kb.aora',
     projectId: '6680cff0003cb623a99d',
-    databaseId: '6680d21f0017fd8082ec'
+    databaseId: '6680d21f0017fd8082ec',
+    userCollectionId: '6680dfaf000fe3bf743b',
+    videoCollectionId: '6680dfef001db31f81a3',
+    storageId: '6680e16000149f4ea373'
 }
