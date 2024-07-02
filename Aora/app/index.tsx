0f13c9c1dcb945d9ebbf46b493186fde17868e66
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-07-01 17:04:37 -0700

    Added a redirect so if a user is already logged in and hasn't logged out, when they open the app, they're logged in

--- Aora/app/index.tsx
+++ Aora/app/index.tsx
@@ -4,8 +4,14 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../constants';
 import CustomButton from '@/components/CustomButton';
+import { useGlobalContext } from '../context/GlobalProvider';
 
 export default function HomeScreen() {
+
+    const {isLoading, isLoggedIn } = useGlobalContext();
+
+    if(!isLoading && isLoggedIn) return <Redirect href='/home' />
+
     return (
         <SafeAreaView className='bg-primary h-full'>
             <ScrollView contentContainerStyle={{ height: '100%'}}>
