c80cf8a2d0c82a3cb1ed90af261d1e1f58e25abf
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-07-02 13:18:19 -0700

    homepage design, empty page design, trending videos section outline

--- Aora/app/(tabs)/home.tsx
+++ Aora/app/(tabs)/home.tsx
@@ -1,12 +1,23 @@
-import { View, Text, FlatList, Image } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, Image, RefreshControl } from 'react-native'
+import React, {useState} from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import SearchInput from '../../components/SearchInput'
+import Trending from '../../components/Trending'
+import EmptyState from '../../components/EmptyState'
 
 const Home = () => {
+
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = async () => {
+        setRefreshing(true);
+
+        setRefreshing(false);
+    }
+
     return (
-        <SafeAreaView className='bg-primary'>
+        <SafeAreaView className='bg-primary h-full'>
             <FlatList 
               data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
               keyExtractor={(item) => item.id.toString()}
@@ -15,6 +26,7 @@ const Home = () => {
               )}
               ListHeaderComponent={() => (
                 <View className='my-6 px-4 space-y-6'>
+                    {/* Top part of screen with username and logo */}
                     <View className='justify-between items-start flex-row mb-6'>
                         <View>
                             <Text className='font-pmedium text-sm text-gray-100'>
@@ -33,13 +45,26 @@ const Home = () => {
                         </View>
                     </View>
 
-                    <SearchInput/>
+                    {/* Next section with the search bar */}
+                    <SearchInput />
 
-                    <View>
-                        
+                    {/* Section with most popular videos */}
+                    <View className='w-full flex-1 pt-5 pb-8'>
+                        <Text className='text-gray-100 text-lg font-pregular mb-3'>
+                            Latest Videos
+                        </Text>
+                        <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []}/>
                     </View>
                 </View>
               )}
+              ListEmptyComponent={() => (
+                <EmptyState
+                    title="No Videos Found"
+                    subtitle="Be the first one to upload a video"
+                />
+                // <Text className='text-white'>Empty</Text>
+              )}
+              refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}
             />
         </SafeAreaView>
     )
