ae021e91c937ce2e27e89cc34f69374457b51b95
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-07-09 17:19:08 -0700

    create video page layout

--- Aora/app/(tabs)/create.tsx
+++ Aora/app/(tabs)/create.tsx
@@ -1,11 +1,114 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import FormField from '../../components/FormField'
+import { Video, ResizeMode } from 'expo-av'
+import { icons } from '../../constants'
+import CustomButton from '../../components/CustomButton'
 
 const Create = () => {
+
+    const [uploading, setUploading] = useState(false)
+
+    const [form, setForm] = useState({
+        title: '',
+        video: null,
+        thumbnail: null,
+        prompt: ''
+    })
+
+    const submit = () = {}
+
     return (
-        <View>
-            <Text>Create</Text>
-        </View>
+        <SafeAreaView className='bg-primary h-full'>
+            <ScrollView className='px-4 my-6'>
+                <Text className='text-2xl text-white font-psemibold'>
+                    Upload Video
+                </Text>
+
+                <FormField 
+                    title="Video Title"
+                    value={form.title}
+                    placeholder="Give your video a catchy title..."
+                    handleChangeText={(e: string) => setForm({...form, title: e})}
+                    otherStyles="mt-10"
+                />
+
+                {/* Space for user to upload their video */}
+                <View className='mt-7 space-y-2'>
+                    <Text className='text-base text-gray-100 font-pmedium'>
+                        Upload Video
+                    </Text>
+
+                    <TouchableOpacity>
+                        {form.video ? (
+                            <Video 
+                            // @ts-ignore: Ignore the type error for now
+                                source={{ uri: form.video.uri }}
+                                className='w-full h-64 rounded-2xl'
+                                useNativeControls
+                                resizeMode={ResizeMode.COVER}
+                                isLooping
+                            />
+                        ) : (
+                            <View className='w-full h-40 px-4 bg-black-100 rounded-2xl justify-center items-center'>
+                                <View className='w-14 h-14 border border-dashed border-secondary-100 justify-center items-center'>
+                                    <Image 
+                                        source={icons.upload}
+                                        resizeMode='contain'
+                                        className='w-1/2 h-1/2'
+                                    />
+                                </View>
+                            </View>
+                        )}
+                    </TouchableOpacity>
+                </View>
+                
+                <View className='mt-7 space-y-2'>
+                    <Text className='text-base text-gray-100 font-pmedium'>
+                        Thumbnail Image
+                    </Text>
+
+                    <TouchableOpacity>
+                        {form.thumbnail ? (
+                            <Image 
+                            // @ts-ignore: Ignore the type error for now
+                                source={{ uri: form.thumbnail.uri }}
+                                className='w-full h-64 rounded-2xl'
+                                resizeMode='cover'
+                            />
+                        ) : (
+                            <View className='w-full h-16 px-4 bg-black-100 rounded-2xl justify-center items-center border-2 border-black-200 flex-row space-x-2'>
+                                    <Image 
+                                        source={icons.upload}
+                                        resizeMode='contain'
+                                        className='w-5 h-5'
+                                    />
+                                    <Text className='text-sm text-gray-100 font-pmedium'>
+                                        Choose a file
+                                    </Text>
+                                </View>
+                        )}
+                    </TouchableOpacity>
+                </View>
+
+                <FormField 
+                    title="AI Prompt"
+                    value={form.prompt}
+                    placeholder="The prompt you used to create this video"
+                    handleChangeText={(e: string) => setForm({...form, prompt: e})}
+                    otherStyles="mt-7"
+                />
+
+                <CustomButton 
+                    title="Submit & Publish"
+                    handlePress={submit}
+                    containerStyles="mt-7"
+                    isLoading={uploading}
+                />
+
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
